a2717d1f5e3c881a1b1327086de8e9670e615a86
Nyv <79833594+supernyv@users.noreply.github.com>
2021-09-16 04:17:04 +0100

    Add files via upload

--- scripts/sound_effects.py
+++ scripts/sound_effects.py
@@ -2,23 +2,21 @@ import pygame
 pygame.mixer.pre_init(20070, -16, 2, 1024) #(frequency in Hz, Bitdepth, Number of channels 1 for mono and 2 stereo, buffer size
 pygame.mixer.init()
 
-replace_sound = pygame.mixer.Sound('sounds/futuristic-slide.wav')
+replace_sound = pygame.mixer.Sound('sounds/replace.wav')
 replace_sound.set_volume(0.1)
-undo_sound = pygame.mixer.Sound('sounds/sci-fi-sweep.wav')
+undo_sound = pygame.mixer.Sound('sounds/undo.wav')
 undo_sound.set_volume(0.5)
 
-valid_word_sound = pygame.mixer.Sound('sounds/page-forward.wav')
+valid_word_sound = pygame.mixer.Sound('sounds/valid.wav')
 valid_word_sound.set_volume(0.5)
-invalid_word_sound = pygame.mixer.Sound('sounds/game-buzz.wav')
+invalid_word_sound = pygame.mixer.Sound('sounds/invalid.wav')
 invalid_word_sound.set_volume(0.7)
 
-placement_sound = pygame.mixer.Sound('sounds/click-error.wav')
+placement_sound = pygame.mixer.Sound('sounds/wrong_placement.wav')
 placement_sound.set_volume(0.5)
 
-wrong_start_sound = pygame.mixer.Sound('sounds/single-key-type.wav')
-
-drop_letter_sound = pygame.mixer.Sound('sounds/single-key-type.wav')
+drop_letter_sound = pygame.mixer.Sound('sounds/drop.wav')
 drop_letter_sound.set_volume(0.5)
 
-new_game_sound = pygame.mixer.Sound('sounds/ominous-drums.wav')
+new_game_sound = pygame.mixer.Sound('sounds/new_game.wav')
 new_game_sound.set_volume(0.5)
